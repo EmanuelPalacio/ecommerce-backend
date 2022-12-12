d440f7650d573b4337d6fcfd0ae27812e7e19e39
Emanuel Palacio <emanuel.palacio.arq@gmail.com>
2022-12-12 11:51:37 -0300

    delete cart

--- server/src/routes/cart.ts
+++ server/src/routes/cart.ts
@@ -1,12 +1,17 @@
 import express from 'express';
-import { getCart, postCart, addProductsCart } from '../controllers/cart';
+import {
+	getCart,
+	postCart,
+	addProductsCart,
+	deleteCart,
+} from '../controllers/cart';
 
 const router = express.Router();
 
 router.get('/:id/products', getCart);
 router.post('/', postCart);
 router.post('/:id/products', addProductsCart);
-router.delete('/:id');
+router.delete('/:id', deleteCart);
 router.delete('/:id/products/:id_prod');
 
 export default router;
