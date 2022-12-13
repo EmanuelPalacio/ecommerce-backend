7b82f8556567d73fa0763948b2d49a92da85c046
Emanuel Palacio <emanuel.palacio.arq@gmail.com>
2022-12-12 21:01:57 -0300

    fix

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -6,5 +6,10 @@ const router = express.Router();
 
 router.use('/api/products', products);
 router.use('/api/cart', cart);
+router.use('/*', (_req, res) => {
+	res.status(404).json({
+		msg: 'no existe la ruta',
+	});
+});
 
 export { router };
